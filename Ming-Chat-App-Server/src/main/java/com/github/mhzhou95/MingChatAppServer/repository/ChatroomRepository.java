b3bd075893c7cd2de2a9e28f3bf0767f120186e4
Minghao <minghaozhou123@gmail.com>
2020-02-24 16:35:23 -0500

    removed folders

--- Ming-Chat-App-Server/src/main/java/com/github/mhzhou95/MingChatAppServer/repository/ChatroomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.mhzhou95.MingChatAppServer.repository;
-
-import com.github.mhzhou95.MingChatAppServer.model.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatroomRepository extends JpaRepository<ChatRoom, Long> {
-}
--- src/main/java/com/github/mhzhou95/MingChatAppServer/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.mhzhou95.MingChatAppServer.repository;
-
-import com.github.mhzhou95.MingChatAppServer.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
